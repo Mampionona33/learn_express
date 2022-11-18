b54ee1b0523f70d5701ca1fb60c373259e368068
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2022-11-18 14:32:57 +0000

    setup simple routing

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // inport module with require
 const http = require("http");
+const url = require("url");
 
 // create server using createServer methode
 // and past a response for all request
 const server = http.createServer((req, res) => {
-  // sending back a simple respon from request
-  console.log(req);
-  res.end(`Hello from the server`);
+  // request url is /  and /favicon.ico by default
+  // console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    // if path is equal to /  or /overview, then send respons :
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    // this header alwas to be set before sending response
+    res.writeHead(404, {
+      "content-type": "text/html", //format the respons to html
+      "my-own-header": "helllo-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
 const PORT = 8000;
