b49eb1b5bd65c6bc6454c93f9d60eaa3622bc030
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2022-11-18 15:03:19 +0000

    create simple api

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // inport module with require
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+
+// read the fille first to avoid to read it each time someone request it
+// it's executed onece on the beging
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
 
 // create server using createServer methode
 // and past a response for all request
@@ -15,6 +21,11 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {     
+      "Content-type": "application/json",  // format respons as json
+    });
+    res.end(data);
   } else {
     // this header alwas to be set before sending response
     res.writeHead(404, {
