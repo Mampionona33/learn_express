c7800eba470c82e030ae1c8f65a8355cfdea3d39
Mampionona RAMAHAZOMANANA <ramamps33@gmail.com>
2022-11-23 11:51:19 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const replaceTemplate = (template, product) => {
   output = output.replace(/{%FROM%}/g, product.from);
   output = output.replace(/{%FROM%}/g, product.from);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTIF%}/g, product.descriptif);
 
   return output;
 };
