051606ff3d128f93d1c644bdbe516b3b80b3712e
Mampionona33 <ramamps33@gmail.com>
2022-12-04 20:06:42 +0300

    commit change in app.js

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from server', app: 'My app' });
+// expres.json() is a middleware to modify the incomming request
+// If we do not use it, so the data from the methode post will be undefined
+app.use(express.json());
+
+// Read data from local data base
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// GET all tours from database
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'succes', result: tours.length, data: { tours: tours } });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post from this url');
+// Add new tours to database
+app.post('/api/v1/tours', (req, res) => {
+  /* 
+    in a real data base the id will be create automaticaly
+    but in this facke data , we will create a id
+  */
+  const newId = tours[tours.length - 1]._id + 1;
+  console.log(newId);
+  const newTour = Object.assign({ _id: newId }, req.body);
+  console.log(newTour);
+  tours.push(newTour);
+  // use writeFile not writeFileSync inside callback function
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const PORT = 3000;
