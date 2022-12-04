f373cb66e1cc49a9a010569f2ef7286c831b4e41
Mampionona33 <ramamps33@gmail.com>
2022-12-04 15:36:12 +0300

    getting response of async in then

--- index.js
+++ index.js
@@ -19,36 +19,28 @@ const writeFilePromise = (file, data) => {
   });
 };
 
-// Use promise to prevent calback hell > form of calbacks
-// use return at the end of each then
-
-// readFilePromise(`${__dirname}/input.txt`)
-//   .then((data) => {
-//     const url = `https://random.imagecdn.app/500/${data}`;
-//     return superagent.get(url);
-//   })
-//   .then((res) => {
-//     return writeFilePromise(
-//       `${__dirname}/output.txt`,
-//       res.redirects[1],
-//       "utf8"
-//     );
-//   })
-//   .then(() => console.log("File successfully create"))
-//   .catch((error) => {
-//     console.log(error.message);
-//   });
-
-// this code is work as before but it is more readable
-
 const getPic = async () => {
-  const data = await readFilePromise(`${__dirname}/input.txt`);
-  console.log(data);
-
-  const url = `https://random.imagecdn.app/500/${data}`;
-  const res = await superagent.get(url);
-  console.log(res.redirects[1]);
-  await writeFilePromise(`${__dirname}/output.txt`, res.redirects[1], "utf8");
+  try {
+    const data = await readFilePromise(`${__dirname}/input.txt`);
+    console.log(data);
+    const url = `https://random.imagecdn.app/500/${data}`;
+    const res = await superagent.get(url);
+    console.log(res.redirects[1]);
+    await writeFilePromise(`${__dirname}/output.txt`, res.redirects[1], "utf8");
+  } catch (error) {
+    console.log(error.message);
+    // using throw to return error as respons of promise when there is error
+    throw error.message;
+  }
+  return "2: ready";
 };
 
-getPic();
+console.log("1- Will get pic");
+getPic()
+  .then((res) => {
+    console.log(res);
+    console.log("3- task done");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
