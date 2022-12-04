1ceb9b16ea48da475db92bed2cc739e3bb05cfab
Mampionona33 <ramamps33@gmail.com>
2022-12-04 15:07:34 +0300

    rewrite the code with async await

--- index.js
+++ index.js
@@ -22,19 +22,33 @@ const writeFilePromise = (file, data) => {
 // Use promise to prevent calback hell > form of calbacks
 // use return at the end of each then
 
-readFilePromise(`${__dirname}/input.txt`)
-  .then((data) => {
-    const url = `https://random.imagecdn.app/500/${data}`;
-    return superagent.get(url);
-  })
-  .then((res) => {
-    return writeFilePromise(
-      `${__dirname}/output.txt`,
-      res.redirects[1],
-      "utf8"
-    );
-  })
-  .then(() => console.log("File successfully create"))
-  .catch((error) => {
-    console.log(error.message);
-  });
+// readFilePromise(`${__dirname}/input.txt`)
+//   .then((data) => {
+//     const url = `https://random.imagecdn.app/500/${data}`;
+//     return superagent.get(url);
+//   })
+//   .then((res) => {
+//     return writeFilePromise(
+//       `${__dirname}/output.txt`,
+//       res.redirects[1],
+//       "utf8"
+//     );
+//   })
+//   .then(() => console.log("File successfully create"))
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
+
+// this code is work as before but it is more readable
+
+const getPic = async () => {
+  const data = await readFilePromise(`${__dirname}/input.txt`);
+  console.log(data);
+
+  const url = `https://random.imagecdn.app/500/${data}`;
+  const res = await superagent.get(url);
+  console.log(res.redirects[1]);
+  await writeFilePromise(`${__dirname}/output.txt`, res.redirects[1], "utf8");
+};
+
+getPic();
