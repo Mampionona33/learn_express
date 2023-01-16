e5b1fe1eca52c184de652758ca5f48e8509c4acf
Mampionona33 <ramamps33@gmail.com>
2023-01-16 18:15:24 +0300

    create users controllers and routers

--- app.js
+++ app.js
@@ -42,8 +42,14 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/users-simple.json`)
+);
+
 /* ********************************
  **************ROUTE CONTROLLERS */
+
+// -----------tours controllers ---------------
 const getTours = (req, res) => {
   res.status(200).json({
     status: 'succes',
@@ -161,13 +167,128 @@ const deleteTour = (req, res) => {
   );
 };
 
+// --------------users controlers -------------------
+const getUsers = (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    'requested at': req.requestTime,
+    result: users.length,
+    data: { users: users },
+  });
+};
+
+const createUser = (req, res) => {
+  /* 
+    in a real data base the id will be create automaticaly
+    but in this facke data , we will create a id
+  */
+  const newId = users[users.length - 1]._id + 1;
+  console.log(newId);
+  const newUser = Object.assign({ _id: newId }, req.body);
+  console.log(newUser);
+  users.push(newUser);
+  // use writeFile not writeFileSync inside callback function
+  fs.writeFile(
+    `${__dirname}/dev-data/data/users-simple.json`,
+    JSON.stringify(users),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          user: newUser,
+        },
+      });
+    }
+  );
+};
+
+const getUser = (req, res) => {
+  // to create optional parameter add ? after params like : /api/v1/users/:id?
+  // params are variables from url
+  const id = req.params.id * 1;
+
+  if (id > users.length - 1) {
+    return res.status(404).json({ message: 'Invalid id' });
+  }
+
+  const user = users.find((el) => el._id === id);
+  res.status(200).json({
+    status: 'succes',
+    data: { user: user },
+  });
+};
+
+const updateUser = (req, res) => {
+  const id = req.params.id * 1;
+  const selectedUser = tours.filter((el) => el._id === id);
+  const updatedUsers = tours.filter((el) => el._id !== id);
+
+  const updeatedTour = selectedUser.map((el) => {
+    return Object.assign({}, el, req.body);
+  });
+  updatedUsers.push(updeatedTour[0]);
+
+  console.log(selectedUser);
+
+  // Validation
+  if (id > tours.length - 1 || selectedUser.length <= 0) {
+    console.log(id, tours.length);
+    return res.status(404).json({ message: 'Invalid id' });
+  }
+
+  // update data after passing validation
+  fs.writeFile(
+    `${__dirname}/dev-data/data/users-simple.json`,
+    JSON.stringify(updatedUsers),
+    (err) => {
+      res.status(201).json({
+        status: 'succes',
+        message: `User : ${id} has been successfully update`,
+      });
+      if (err) {
+        return res.status(404).json({
+          status: 'failed',
+          message: 'error while trying to update the data',
+        });
+      }
+    }
+  );
+};
+
+const deleteUser = (req, res) => {
+  const id = req.params.id * 1;
+  const updatedUser = users.filter((el) => el._id !== id);
+
+  console.log(updatedUser);
+
+  // Validation
+  if (id > users.length - 1) {
+    return res.status(404).json({ message: 'Invalid id' });
+  }
+
+  // update data after passing validation
+  fs.writeFile(
+    `${__dirname}/dev-data/data/users-simple.json`,
+    JSON.stringify(updatedUser),
+    (err) => {
+      res.status(201).json({
+        status: 'succes',
+        message: `user : ${id} has been successfully deleted`,
+      });
+      if (err) {
+        return res.status(404).json({
+          status: 'failed',
+          message: 'error while trying to update the data',
+        });
+      }
+    }
+  );
+};
+
 /* *******************************
  *************************ROUTES */
+
 // GET all tours from database
-
-const toursBasedUrl = '/api/v1/tours';
-const tourByIdBasedUrl = '/api/v1/tours/:id';
-
 // app.get('/api/v1/tours', getTours);
 
 // GET one tour from database
@@ -182,8 +303,17 @@ const tourByIdBasedUrl = '/api/v1/tours/:id';
 // Delete tour
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// ------------- Tours routes ----------------
+const toursBasedUrl = '/api/v1/tours';
+const tourByIdBasedUrl = '/api/v1/tours/:id';
 app.route(toursBasedUrl).get(getTours).post(createTour);
 app.route(tourByIdBasedUrl).get(getTour).patch(updateTour).delete(deleteTour);
+
+// ------------------- User Routes -------------------
+const userBasedUrl = '/api/v1/users';
+const userByIdBasedUrl = '/api/v1/users/:id';
+app.route(userBasedUrl).get(getUsers).post(createUser);
+app.route(userByIdBasedUrl).get(getUser).patch(updateUser).delete(deleteUser);
 
 const PORT = 3000;
 
