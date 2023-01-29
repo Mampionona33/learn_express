fcdc32d266d57079fa4dfacfe5590bfc2b8b36a6
Mampionona33 <ramamps33@gmail.com>
2023-01-29 17:22:34 +0300

    finish to modify tour model

--- models/toursModel.js
+++ models/toursModel.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 // Schema is used to validate data
 const tourSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'Name is required'], unique: true },
-
+  duration: { type: Number, required: [true, 'Duration is required'] },
   description: { type: String, required: [true, 'Description is required'] },
+  imageCover: { type: String, required: [true, 'Image cover is required'] },
+  images: [String],
+  createAt: { type: Date, default: Date.now() },
 });
 
 // create a basic tour model
