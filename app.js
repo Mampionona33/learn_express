381b03e52d266904ae528c6a04ef0c1ad85ca558
Mampionona33 <ramamps33@gmail.com>
2023-01-29 18:08:39 +0300

    move mongodb connection from app.js to server.js

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
 const express = require('express');
 
-const mongoose = require('mongoose');
-
 const app = express();
-const morgan = require('morgan');
+
 const dotenv = require('dotenv');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-/*
-  In Node.js, an environment variable is a variable that is set outside of the code and can be accessed within the application. 
-  The ".env" file is a file that stores these environment variables in key-value pairs, and is commonly used to store sensitive 
-  information such as database credentials or API keys. 
-  The variables in the .env file can be accessed using the "dotenv" package, which allows the application to read and use the 
-  variables stored in the file. 
-  It is important to note that the .env file should not be committed to version control as it contains sensitive information.
- */
 dotenv.config({ path: './.env' });
-
-const DB = process.env.DATABASE_URL.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-// Connecting app to database
-mongoose.connect(DB).then(() => console.log('DB connection successful !'));
-
-/* ********************************
- ********************MIDDLEWARES */
-// morgan is HTTP request logger middleware for node.js
-// Only run the morgan middleware if Node environment is set to development
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
 // expres.json() is a middleware to modify the incomming request
 // If we do not use it, so the data from the methode post will be undefined
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = require('./app');
 
+/*
+  In Node.js, an environment variable is a variable that is set outside of the code and can be accessed within the application. 
+  The ".env" file is a file that stores these environment variables in key-value pairs, and is commonly used to store sensitive 
+  information such as database credentials or API keys. 
+  The variables in the .env file can be accessed using the "dotenv" package, which allows the application to read and use the 
+  variables stored in the file. 
+  It is important to note that the .env file should not be committed to version control as it contains sensitive information.
+ */
 dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+/* ********************************
+ ********************MIDDLEWARES */
+// morgan is HTTP request logger middleware for node.js
+// Only run the morgan middleware if Node environment is set to development
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Connecting app to database
+mongoose.connect(DB).then(() => console.log('DB connection successful !'));
 
 // -------- Run server -----------
 const PORT = process.env.PORT || 3000;
