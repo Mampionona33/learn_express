7ded2aa0d5f099c41724987deb44e084cd42a07a
Mampionona33 <ramamps33@gmail.com>
2023-01-29 08:16:58 +0300

    adding port variable from .env to server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
+dotenv.config({ path: './.env' });
+
 // -------- Run server -----------
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
