dc5052a8c8f39af5519a3b5dc7b1080c86cdc2eb
Mampionona33 <ramamps33@gmail.com>
2023-02-04 21:12:20 +0300

    Refactoring global handler

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
-
 const dotenv = require('dotenv');
+const AppErro = require('./utiles/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -54,20 +55,16 @@ app.use(userBasedUrl, userRouter);
   that means, it pass through previous routes
 */
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl}`,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl}`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl}`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
   /* 
     if next function receve some argument, express
     know that it is an error. And it will skip throug
     all other middleware stack and 
   */
-  next(err);
+  next(new AppErro(`Can't find ${req.originalUrl}`, 404));
 });
 
 // HANDLING ERROR GLOBAL MIDDLEWARE
@@ -75,16 +72,6 @@ app.all('*', (req, res, next) => {
   By specifing four parameter, express know that
   this is a error handling middleware
 */
-
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-  next();
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ utiles/appError.js
@@ -0,0 +1,13 @@
+class AppErro extends Error {
+  constructor(message, statusCode) {
+    // message is the only parameter that the error accep
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppErro;
