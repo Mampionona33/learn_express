f5bd59793056caae8e24d2b75a3cfa3e1529599b
mampionona33 <ramamps33@gmail.com>
2023-02-09 13:32:31 +0300

    handling duplicate field from mongodb

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -33,6 +33,12 @@ const handleCastErrorDB = (err) => {
   return new AppErro(message, 400);
 };
 
+const handleDuplicateFielsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value : ${value}. Please use another value!`;
+  return new AppErro(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -42,10 +48,12 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     // do not use let error = {...err}; to make a copy of the erro
     // because it will not copy the name
-    let error = { ...err, name: err.name };
+    let error = { ...err, name: err.name, errmsg: err.errmsg };
 
     // Handling _id error
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // handling duplicate field
+    if (error.code === 11000) error = handleDuplicateFielsDB(error);
 
     sendErrorProduction(error, res);
   }
