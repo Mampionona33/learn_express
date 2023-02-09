cc6d648764fabec88172912a87609ed9b0abd8d4
mampionona33 <ramamps33@gmail.com>
2023-02-09 08:39:52 +0300

    create diffrent error handling for production and development env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,38 @@
-module.exports = (err, req, res, next) => {
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProduction = (err, res) => {
+  // Operational, trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error : don't leak error details
+  } else {
+    // 1) log error
+    console.error('ERROR', err);
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-  next();
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res);
+  }
 };
