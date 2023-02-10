1ad8ca8656fec447b778e57bc367baac2f46ebc8
Mampionona33 <ramamps33@gmail.com>
2023-02-10 22:00:46 +0300

    create new user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,12 @@ const {
   getUsers,
   updateUser,
 } = require('../controllers/usersControllers');
+const authController = require('../controllers/authController')
 
 // const userRouter = express.Router();
 const router = express.Router();
+router.post('/signup', authController.signup);
+
 router.route('/').get(getUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
