59b3e490ffd606a471a089c3a52ae13877478aa6
mampionona33 <ramamps33@gmail.com>
2023-02-11 14:11:00 +0300

    format authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,12 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utiles/catchAsync');
 
-exports.signup = catchAsync( async (req ,res, next)=>{
-    const newUser = await User.create(req.body);
-    res.status(201).json({
-        status:'success',
-        data:{
-            user: newUser
-        }
-    })
-})
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
